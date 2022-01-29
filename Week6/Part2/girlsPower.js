f9c6ffeb1056f7354dafcde9c83eafb056d4bbdb
Cansu Yanık <cansuyanik@hotmail.com>
2022-01-29 19:31:15 +0300

    Update file

--- Week6/Part2/girlsPower.js
+++ Week6/Part2/girlsPower.js
@@ -1,10 +1,10 @@
 
 //higher order function
-girlsPowerFunc = item => (item/2.0) + 3;
+const girlsPowerSum = item => (item/2.0) + 3;
 
 const girlsPower = (arr) => {
     //call higher order function
-    return arr.map(girlsPowerFunc);
+    return arr.map(girlsPowerSum);
 };
 
 let array = [2,3,4]; //example array
